3faef0c920a5a9beacaab364003745bf16352860
MatiasGomez <gomezmati@hotmail.com>
2022-11-21 20:51:24 -0300

    3er entrega tp final

---
+++ js/login.js
@@ -0,0 +1,29 @@
+const btnIngresar=document.querySelector("#ingresar"),
+    user=document.querySelector("#user"),
+    pass=document.querySelector("#pass"),
+    check=document.querySelector("#recordar");
+
+function inicioSesion(usuarios){
+    let userFound= usuarios.find(usuario=>{
+        return usuario.usuario == user.value && usuario.password== pass.value
+    })
+if(userFound){
+    window.location.href="./ejercicios.html"
+}
+else{
+    document.querySelector("#mensaje").innerText= "Usuario y/o contraseña incorrecta";
+}
+    return userFound;
+}
+
+function recuperarLS(){
+    let datos= JSON.parse(localStorage.getItem("usuarios"));
+    return datos;
+}
+
+const usuariosLS= recuperarLS();
+
+btnIngresar.addEventListener("click", (e)=>{
+    e.preventDefault();
+    inicioSesion(usuariosLS)
+});
---
+++ js/register.js
@@ -0,0 +1,47 @@
+const register= document.querySelector(".register"),
+ userReg=document.querySelector("#userReg"),
+ passReg=document.querySelector("#passReg"),
+ email=document.querySelector("#email"),
+ btnRegistrar=document.querySelector("#registrar");
+
+ let usuarios;
+ if (localStorage.getItem("usuarios")){
+    usuarios= JSON.parse(localStorage.getItem("usuarios"));
+}
+else{usuarios=[];}
+
+/* constructor usuario */
+class Usuario{
+    constructor(usuario,password,email){
+        this.usuario=usuario;
+        this.password=password;
+        this.email=email;
+    }
+}
+
+function limpiarCampos(){
+    userReg.value="";
+    passReg.value="";
+    email.value="";
+}
+
+function guardarUsuario(usuario){
+    return usuarios.push(usuario);
+}
+
+function guardarEnStorage (usuario){
+    let ls=localStorage.setItem("usuarios",JSON.stringify(usuario));
+    return ls;
+}
+
+btnRegistrar.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let newUser= new Usuario(
+        userReg.value,
+        passReg.value,
+        email.value
+    );
+    guardarUsuario(newUser);
+    limpiarCampos();
+    guardarEnStorage(usuarios);
+});
